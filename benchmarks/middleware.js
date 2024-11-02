f8469f8aed146d0c6ba03f2daf33e5f0ba3b9bb7
tchapacan <fabrice.orsolini@gmail.com>
2024-11-02 16:34:23 +0100

    kickstart repo loadtest wf

---
+++ benchmarks/middleware.js
@@ -0,0 +1,20 @@
+
+const express = process.env.NO_LOCAL_EXPRESS ? require('express') : require('..');
+const app = express();
+
+// number of middleware
+
+let n = parseInt(process.env.MW || '1', 10);
+console.log('  %s middleware', n);
+
+while (n--) {
+  app.use(function(req, res, next){
+    next();
+  });
+}
+
+app.use(function(req, res){
+  res.send('Hello World')
+});
+
+app.listen(3333);
